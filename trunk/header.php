a1a544482359b893333a1b2b9f3310bb0ba0070b
nicholas.rathmann <nicholas.rathmann@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-04-25 13:02:14 +0000

    RSS should now also work when non-valid XML chars are contained in posts.

--- trunk/header.php
+++ trunk/header.php
@@ -25,7 +25,7 @@
  *   General
  *********************/
 
-define('OBBLM_VERSION', '0.38l');
+define('OBBLM_VERSION', '0.38m');
 $credits = array('Pierluigi Masia', 'Mag Merli', 'Lars Scharrenberg', 'Tim Haini', 'Daniel Straalman', 'Juergen Unfried', 'Sune Radich Christensen');
 define('MAX_RECENT_GAMES', 15); // This limits the number of rows shown in the "recent games" tables.
 define('MAX_MEM_MATCHES', 3); // For each mem. match category: If the number of matches with equal records exceed this value, no matches are shown at all.
