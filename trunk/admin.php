3f8b9ad98b48164f0c745a9305915e009234aee1
Nimda562 <Nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-05-24 10:44:55 +0000

    Made some changes in admin->tour. handling section to make the tour. edit feature more obviously a tournament editor than creator.

--- trunk/admin.php
+++ trunk/admin.php
@@ -857,98 +857,114 @@ function sec_admin() {
         }
 
         title($lng->getTrn('secs/admin/th'));
+        $tours = Tour::getTours();
         $nameChangeJScode = "e = document.forms['tourForm'].elements; e['tname'].value = e['trid'].options[e['trid'].selectedIndex].text;";
         
         ?>
-        <form id='tourForm' method="POST">
-            <b>Tournament:</b><br>
-            <select name="trid" onChange="<?php echo $nameChangeJScode;?>">
-                <?php
-                foreach (Tour::getTours() as $t) {
-                    echo "<option value='$t->tour_id'>$t->name</option>\n";
-                }
-                ?>
-            </select>
+        <div class="adminBox">
+            <div class="boxTitle3"><?php echo $lng->getTrn('secs/admin/edit_tour');?></div>
+            <div class="boxBody">
+            <form id='tourForm' method="POST">
+                <br>
+                <b>Edit tournament:</b><br>
+                <select name="trid" onChange="<?php echo $nameChangeJScode;?>">
+                    <?php
+                    foreach ($tours as $t) {
+                        echo "<option value='$t->tour_id'>$t->name</option>\n";
+                    }
+                    ?>
+                </select>
+                <br><br>
+                <hr>
+                <br>
+                <b>New name:</b><br>
+                <input type='text' name='tname' length='20' value=''>
 
-            <br><br>
-            <b>Name:</b><br>
-            <input type='text' name='tname' length='20' value=''>
+                <script language="JavaScript" type="text/javascript">
+                    <?php echo $nameChangeJScode;?>
+                </script>
 
-            <script language="JavaScript" type="text/javascript">
-                <?php echo $nameChangeJScode;?>
-            </script>
+                <br><br>
+                <b>New ranking system:</b> (<?php echo $lng->getTrn('secs/admin/prefixes');?>)<br>
+                <select name='rs'>
+                <?php
+                foreach (Tour::getRSSortRules(false, true) as $idx => $r) {
+                    echo "<option value='$idx'>RS #$idx | $r</option>\n";
+                }
+                ?>
+                </select>
+               
+                <br><br>
+                <b>New tournament type:</b> <?php echo $lng->getTrn('secs/admin/conv_warn');?><br>
+                <input type="radio" name="ttype" value="<?php echo TT_NOFINAL;?>" > Round-Robin without final<br>
+                <input type="radio" name="ttype" value="<?php echo TT_FINAL;?>" CHECKED> Round-Robin with final<br>
+                <input type="radio" name="ttype" value="<?php echo TT_SEMI;?>" > Round-Robin with final and semi-finals<br>
+                <input type="radio" name="ttype" value="<?php echo TT_KNOCKOUT;?>" > Knock-out (AKA. single-elimination, cup, sudden death)<br>
+                <input type="radio" name="ttype" value="<?php echo TT_SINGLE;?>" > FFA (free for all) single match<br>
+                <br>
+                
+                <input type="hidden" name="type" value="change">
+                <input type="submit" value="Submit changes" <?php echo (empty($tours)) ? 'DISABLED' : ''?>>
+                <br>
+            </form>
+            </div>
+        </div>
 
-            <br><br>
-            <b>Ranking system:</b> (<?php echo $lng->getTrn('secs/admin/prefixes');?>)<br>
-            <select name='rs'>
-            <?php
-            foreach (Tour::getRSSortRules(false, true) as $idx => $r) {
-                echo "<option value='$idx'>RS #$idx | $r</option>\n";
-            }
-            ?>
-            </select>
-           
-            <br><br>
-            <b>Tournament type:</b> <?php echo $lng->getTrn('secs/admin/conv_warn');?><br>
-            <input type="radio" name="ttype" value="<?php echo TT_NOFINAL;?>" > Round-Robin without final<br>
-            <input type="radio" name="ttype" value="<?php echo TT_FINAL;?>" CHECKED> Round-Robin with final<br>
-            <input type="radio" name="ttype" value="<?php echo TT_SEMI;?>" > Round-Robin with final and semi-finals<br>
-            <input type="radio" name="ttype" value="<?php echo TT_KNOCKOUT;?>" > Knock-out (AKA. single-elimination, cup, sudden death)<br>
-            <input type="radio" name="ttype" value="<?php echo TT_SINGLE;?>" > FFA (free for all) single match<br>
-            <br><br>
-            
-            <input type="hidden" name="type" value="change">
-            <input type="submit" value="Submit changes">
-            <br><br>
-        </form>
-        <hr>
-        <form method="POST">
-            <?php title($lng->getTrn('secs/admin/tour_del'));?>
-            <b>I wish to delete the following tournament</b><br>
-            <select name="trid">
-                <?php
-                foreach (Tour::getTours() as $t) {
-                    echo "<option value='$t->tour_id'>$t->name</option>\n";
-                }
-                ?>
-            </select>
-            <br><br>
-            <b><?php echo $lng->getTrn('secs/admin/advise/have_read');?>:</b> 
-            <input type="checkbox" name="delete" value="1">
-            <br><br>
-            <b><u>Advisement/warning:</u></b><br>
-            <?php echo $lng->getTrn('secs/admin/advise/pre');?>
-            <br>
-            <ul>
-                <li><?php echo $lng->getTrn('secs/admin/advise/e1');?></li>
-                <li><?php echo $lng->getTrn('secs/admin/advise/e2');?></li>
-            </ul>
-            <br>           
-            <input type="hidden" name="type" value="delete">
-            <input type="submit" value="Delete" onclick="if(!confirm('Are you absolutely sure you want to delete this tournament?')){return false;}">
-        </form>
-        <hr>
-        <form method="POST">
-            <?php title($lng->getTrn('secs/admin/move_div'));?>
-            <b>Move tournament</b><br>
-            <select name="trid">
-                <?php
-                foreach (Tour::getTours() as $t) {
-                    echo "<option value='$t->tour_id'>$t->name</option>\n";
-                }
-                ?>
-            </select><br><br>
-            <b>...to the division</b><br>
-            <select name="did">
-                <?php
-                foreach (Division::getDivisions() as $d) {
-                    echo "<option value='$d->did'>$d->name</option>\n";
-                }
-                ?>
-            </select><br><br>
-            <input type="hidden" name="type" value="move">
-            <input type="submit" value="Move">
-        </form>
+        <div class="adminBox">
+            <div class="boxTitle3"><?php echo $lng->getTrn('secs/admin/tour_del');?></div>
+            <div class="boxBody">
+            <form method="POST">
+                <b>I wish to delete the following tournament</b><br>
+                <select name="trid">
+                    <?php
+                    foreach ($tours as $t) {
+                        echo "<option value='$t->tour_id'>$t->name</option>\n";
+                    }
+                    ?>
+                </select>
+                <br><br>
+                <b><?php echo $lng->getTrn('secs/admin/advise/have_read');?>:</b> 
+                <input type="checkbox" name="delete" value="1">
+                <br><br>
+                <b><u>Advisement/warning:</u></b><br>
+                <?php echo $lng->getTrn('secs/admin/advise/pre');?>
+                <br>
+                <ul>
+                    <li><?php echo $lng->getTrn('secs/admin/advise/e1');?></li>
+                    <li><?php echo $lng->getTrn('secs/admin/advise/e2');?></li>
+                </ul>
+                <br>           
+                <input type="hidden" name="type" value="delete">
+                <input type="submit" value="Delete" onclick="if(!confirm('Are you absolutely sure you want to delete this tournament?')){return false;}">
+            </form>
+            </div>
+        </div>
+
+        <div class="adminBox">
+            <div class="boxTitle3"><?php echo $lng->getTrn('secs/admin/move_div');?></div>
+            <div class="boxBody">
+            <form method="POST">
+                <b>Move tournament</b><br>
+                <select name="trid">
+                    <?php
+                    foreach ($tours as $t) {
+                        echo "<option value='$t->tour_id'>$t->name</option>\n";
+                    }
+                    ?>
+                </select><br><br>
+                <b>...to the division</b><br>
+                <select name="did">
+                    <?php
+                    foreach (Division::getDivisions() as $d) {
+                        echo "<option value='$d->did'>$d->name</option>\n";
+                    }
+                    ?>
+                </select><br><br>
+                <input type="hidden" name="type" value="move">
+                <input type="submit" value="Move">
+            </form>
+            </div>
+        </div>
         <?php
     }
     elseif (isset($_GET['subsec']) && $_GET['subsec'] == 'log') {
