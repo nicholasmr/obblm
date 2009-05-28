b3f660dd7c96cdffeb769e48eefd1d62bafab71d
funnyfingers@hotmail.com <funnyfingers@hotmail.com@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-05-28 23:19:59 +0000

    Updated form to submit to handler.php.

--- trunk/leegmgr/uploadinc.php
+++ trunk/leegmgr/uploadinc.php
@@ -38,7 +38,7 @@ function uploadpage () {
 
 	Print "
 	<!-- The data encoding type, enctype, MUST be specified as below -->
-	<form enctype='multipart/form-data' action='index.php?section=leegmgr' method='POST'>
+	<form enctype='multipart/form-data' action='handler.php?type=leegmgr' method='POST'>
 	<!-- MAX_FILE_SIZE must precede the file input field -->
 	<input type='hidden' name='MAX_FILE_SIZE' value='30000' />
 	<!-- Name of input element determines name in $_FILES array -->
