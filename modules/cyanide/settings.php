9c11b43d9bf8a7e474cc2ee540957430e63466ed
gregory.rome <gregory.rome@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-08-03 12:34:04 +0000

    module/cyanide: remove non supported races
    Update issue 177
    I'm not sure of the right place of this hack.

--- modules/cyanide/settings.php
+++ modules/cyanide/settings.php
@@ -1,5 +1,33 @@
 <?php
 global $settings;
-$settings['cyanide_enabled'] = true; // Enable Cyanide support. Default is false. @FIXME
-$settings['cyanide_debug'] = true;   // Enable module debug. 
+global $DEA;
+global $raceididx;
+
+$settings['cyanide_enabled'] = true; // Enable Cyanide support. @FIXME Default is false.
+$settings['cyanide_debug']   = true; // Enable module debug.
+
+$settings['cyanide_races'] = array( // Races supported by Cyanide
+	'Chaos',
+	'Dwarf',
+	'Goblin',
+	'Human',
+	'Lizardman',
+	'Orc',
+	'Skaven',
+	'Wood Elf');
+
+if($settings['cyanide_enabled'])
+{
+	foreach ($DEA as $race_name => $attrs) {
+
+		if (!in_array($race_name, $settings['cyanide_races'])) {
+			unset($DEA[$race_name]);
+		}
+	}
+
+	$raceididx = array();
+	foreach (array_keys($DEA) as $race) {
+		$raceididx[$DEA[$race]['other']['race_id']] = $race;
+	}
+}
 ?>
