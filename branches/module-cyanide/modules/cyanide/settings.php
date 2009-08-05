b33db8e034fea1815374f1fc3b96b6826fe1c80c
gregory.rome <gregory.rome@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-08-05 20:33:28 +0000

    module/cyanide: safety commit
    This version should be deployed for test:
     1. Create equal teams in both obblml and cyanide game
     2. Import match reports
     
    Update issue 177
    Import sqlite report implemented.
    TODO:
     * Feature: allow to select the match type before import
     * Feature: Allow to reverse Away/Home for public league

--- branches/module-cyanide/modules/cyanide/settings.php
+++ branches/module-cyanide/modules/cyanide/settings.php
@@ -1,11 +1,20 @@
 <?php
 global $settings;
 
-$settings['cyanide_enabled']       = true; // Enable Cyanide support. @FIXME Default is false.
-$settings['cyanide_debug']         = true; // Enable module debug.
-$settings['cyanide_public_league'] = true; // If true the module is in charge to compute some parameters
+// Enable Cyanide support. @FIXME Default is false.
+$settings['cyanide_enabled']       = true;
 
-$settings['cyanide_races'] = array( // Races supported by Cyanide
+// If true the module is in charge to compute some parameters
+$settings['cyanide_public_league'] = true;
+
+// Uploads report to a scheduled match. The options are [false|true|"strict"]
+// false does not check for scheduled matches
+// true checks for scheduled matches and will create a match if not found
+// "strict" will allow only scheduled matches to be used
+$settings['cyanide_schedule']      = "strict";
+
+// Races supported by Cyanide
+$settings['cyanide_races'] = array(
 	'Chaos',
 	'Dwarf',
 	'Goblin',
