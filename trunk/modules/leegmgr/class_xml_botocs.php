883d796e70a34795dce1a3eb6dba5672dddf1b15
funnyfingers@hotmail.com <funnyfingers@hotmail.com@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-08-12 01:39:33 +0000

    Issue 185 complete.

---
+++ trunk/modules/leegmgr/class_xml_botocs.php
@@ -0,0 +1,210 @@
+<?php
+
+/*
+ *  Copyright (c) William Leonard <email protected> 2009. All Rights Reserved.
+ *
+ *
+ *  This file is part of OBBLM.
+ *
+ *  OBBLM is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OBBLM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class XML_BOTOCS
+{
+    /***************
+     * Properties 
+     ***************/
+
+    public $noninjplayercount = 0;
+    public $exist_journeyman = false;
+    public $jm = 0;
+    //team
+    public $roster = '';
+    public $team_id = 0;
+    public $name = '';
+    public $race = '';
+    public $coach_name = '';
+    public $rerolls = 0;
+    public $fan_factor = 0;
+    public $ass_coaches = 0;
+    public $cheerleaders = 0;
+    public $apothecary = "false";
+    public $treasury = 0;
+    //players
+    public $players;
+
+
+    /***************
+     * Methods 
+     ***************/
+
+    function __construct($team_id, $jm) {
+
+        $this->team_id = $team_id;
+        $this->jm = $jm;
+
+        $team = new Team ( $_GET["teamid"] );
+            $this->players = $team->getPlayers();
+            $this->name = $team->name;
+            $this->race = $team->race;
+            $this->coach_name = $team->coach_name;
+            $this->rerolls = $team->rerolls;
+            $this->fan_factor = $team->fan_factor;
+            $this->ass_coaches = $team->ass_coaches;
+            $this->cheerleaders = $team->cheerleaders;
+            $this->apothecary = $team->apothecary;
+                if ( $this->apothecary == "1" ) $this->apothecary = "true";
+                else $this->apothecary = "false";
+            $this->treasury = $team->treasury;
+            
+        if ( !$this->checkJourneymen() ) return false;
+
+        $this->name = $team->name;
+        $this->coach_name = $team->coach_name;
+        $this->createRoster();
+
+    }
+
+    function createRoster() {
+
+        $this->roster .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        $this->roster .= "<?xml-stylesheet type=\"text/xsl\" href=\"/modules/leegmgr/team.xsl\"?>\n";
+        $this->roster .= "<team>\n";
+        $this->roster .= "    <name>".htmlspecialchars($this->name, ENT_NOQUOTES, 'UTF-8')."</name>\n";
+        $this->roster .= "    <race>".htmlspecialchars($this->race, ENT_NOQUOTES, 'UTF-8')."</race>\n";
+        $this->roster .= "    <coach>".htmlspecialchars($this->coach_name, ENT_NOQUOTES, 'UTF-8')."</coach>\n";
+        $this->roster .= "    <rerolls>".$this->rerolls."</rerolls>\n";
+        $this->roster .= "    <fanfactor>".$this->fan_factor."</fanfactor>\n";
+        $this->roster .= "    <assistants>".$this->ass_coaches."</assistants>\n";
+        $this->roster .= "    <cheerleaders>".$this->cheerleaders."</cheerleaders>\n";
+        $this->roster .= "    <apothecary>".$this->apothecary."</apothecary>\n";
+        $this->roster .= "    <treasury>".$this->treasury."</treasury>\n";
+        $this->roster .= "    <players>\n";
+
+        foreach ( $this->players as $p )
+        {
+
+            if ( !$p->is_dead && !$p->is_sold )
+            {
+                $skills = $p->getSkillsStr();
+                $a_skills = explode(', ', $skills);
+                $this->roster .= "        <player number=\"".$p->nr."\">\n";
+                $this->roster .= "            <name>".htmlspecialchars($p->name, ENT_NOQUOTES, 'UTF-8')."</name>\n";
+                $this->roster .= "            <position>".htmlspecialchars($p->pos, ENT_NOQUOTES, 'UTF-8')."</position>\n";
+                $this->roster .= "            <ma>".$p->ma."</ma>\n";
+                $this->roster .= "            <st>".$p->st."</st>\n";
+                $this->roster .= "            <ag>".$p->ag."</ag>\n";
+                $this->roster .= "            <av>".$p->av."</av>\n";
+                $this->roster .= "            <skills>\n";
+
+                $i = 0;
+
+                    while ( $i < count( $a_skills ) && strlen( $a_skills[0] ) > 0 )
+                    {
+
+                        if ( strpos($a_skills[$i], "Ball & Chain") !== FALSE ) $a_skills[$i] = "Ball and Chain";
+                        if ( strpos($a_skills[$i], "Nurgle's Rot") !== FALSE ) $a_skills[$i] = "Nurgles Rot";
+                        if ( strpos($a_skills[$i], "*") ) $a_skills[$i] = str_replace("*","",$a_skills[$i]);
+				$this->roster .= "                <skill>".htmlspecialchars($a_skills[$i], ENT_NOQUOTES, 'UTF-8')."</skill>\n";
+                        $i++;
+                    }
+                $injured = "false";
+                if ( $p->is_mng ) $injured = "true";
+                $this->roster .= "            </skills>\n";
+                $this->roster .= "            <spp>".$p->spp."</spp>\n";
+                $this->roster .= "            <nigglings>".$p->inj_ni."</nigglings>\n";
+                $this->roster .= "            <injured>".$injured."</injured>\n";
+                $this->roster .= "            <value>".$p->value."</value>\n";
+                $this->roster .= "        </player>\n";
+            }
+
+        }
+
+        $this->roster .= "    </players>\n";
+        $this->roster .= "</team>\n";
+
+    }
+
+    function checkJourneymen()
+    {
+
+        foreach ( $this->players as $p )
+        {
+            if ( !$p->is_dead && !$p->is_sold && $p->is_journeyman && $p->spp > 0 )
+            {
+                Print "You have a journeyman with star players points.  Please hire or fire him.";
+                return false;
+            }
+            if ( !$p->is_dead && !$p->is_sold && !$p->is_mng ) $this->noninjplayercount ++;
+            if ( !$p->is_dead && !$p->is_sold && $p->is_journeyman )
+            {
+                $this->exist_journeyman = true;
+            }
+        }
+	
+        if ( !$this->jm && $this->noninjplayercount < 11 )
+        {
+            Print "You may have forgotten to hire a journeyman for the next match.  Please hire a player as a journeyman or hire a player.<br>";
+            $rosterjm = $this->curPageURL();
+            Print "If you want to ignore this error, use the following for your roster:<br><b>{$rosterjm}</b>";
+            return false;
+        }
+
+        if ( $this->exist_journeyman && $this->noninjplayercount > 11 )
+        {
+            Print "You have a journeyman and more than 11 players that can participate in the next match.";
+            return false;
+        }
+
+        return true;
+
+    }
+
+    function curPageURL() {
+
+        $pageURL = 'http';
+        $pageURL .= "://";
+        $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+        $pageURL .= "&jm=1";
+        return $pageURL;
+
+    }
+
+    public static function main() {
+        
+        // Module registered main function.
+        global $settings;
+        if ( !$settings['leegmgr_enabled'] ) die ("LeegMgr is currently disabled.");
+    
+        if ( isset($_GET["teamid"]) )
+        {
+            $team_id = $_GET["teamid"];
+            if ( isset($_GET["jm"]) ) $jm = $_GET["jm"];
+            else $jm = 0;
+            $roster = new XML_BOTOCS( $team_id, $jm );
+            Print $roster->roster;
+        }
+        else
+        {
+            Print "<html><body>";
+            Print "A valid team id was not found";
+            Print "</body></html>";
+        }
+
+    }
+
+}
+
+?>
