a1ae14798108aaad9a4ccb9ae86f277cbd307b54
gregory.rome <gregory.rome@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-08-14 10:40:04 +0000

    module/cyanide: merge from trunk

--- branches/module-cyanide/modules/modsheader.php
+++ branches/module-cyanide/modules/modsheader.php
@@ -2,7 +2,7 @@
 
 /*
  *  Copyright (c) Nicholas Mossor Rathmann <nicholas.rathmann@gmail.com> 2009. All Rights Reserved.
- *
+ *      
  *
  *  This file is part of OBBLM.
  *
@@ -18,23 +18,23 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *   
  */
 
 /*
     This header is used to register OBBLM modules.
-
+    
     NOTE:
     -----
     filesRunTime are loaded when Module::run() is executed whilest filesLoadTime are loaded when Module::register() is executed.
-*/
+*/ 
 
 Module::register(array(
-    'modname'       => 'inducements',
+    'modname'       => 'inducements', 
     'author'        => 'Daniel Straalman',
     'date'          => '2009',
-    'setCanvas'     => true,
-    'main'          => 'indcPage',
+    'setCanvas'     => true, 
+    'main'          => 'indcPage', 
     'filesRunTime'  => array('inducements/inducements.php'),
     'filesLoadTime' => array()
 ));
@@ -43,8 +43,8 @@ Module::register(array(
     'modname'       => 'pdfroster',
     'author'        => 'Daniel Straalman',
     'date'          => '2008-2009',
-    'setCanvas'     => false,
-    'main'          => 'fpdf_roster',
+    'setCanvas'     => false, 
+    'main'          => 'fpdf_roster', 
     'filesRunTime'  => array('pdf/settings.php', 'pdf/bb_pdf_class.php', 'pdf/pdf_roster.php'),
     'filesLoadTime' => array()
 ));
@@ -53,8 +53,8 @@ Module::register(array(
     'modname'       => 'rss',
     'author'        => 'Juergen Unfried',
     'date'          => '2008',
-    'setCanvas'     => false,
-    'main'          => 'OBBLMRssWriter::main',
+    'setCanvas'     => false, 
+    'main'          => 'OBBLMRssWriter::main', 
     'filesRunTime'  => array('rss/class_rss.php'),
     'filesLoadTime' => array()
 ));
@@ -63,8 +63,8 @@ Module::register(array(
     'modname'       => 'statsgraph',
     'author'        => 'Nicholas Mossor Rathmann',
     'date'          => '2008-2009',
-    'setCanvas'     => false,
-    'main'          => 'SGraph::make',
+    'setCanvas'     => false, 
+    'main'          => 'SGraph::make', 
     'filesRunTime'  => array('statsgraph/class_statsgraph.php'),
     'filesLoadTime' => array('statsgraph/header.php')
 ));
@@ -73,8 +73,8 @@ Module::register(array(
     'modname'       => 'teamxmlexport',
     'author'        => 'Nicholas Mossor Rathmann',
     'date'          => '2009',
-    'setCanvas'     => false,
-    'main'          => 'Team_export::main',
+    'setCanvas'     => false, 
+    'main'          => 'Team_export::main', 
     'filesRunTime'  => array('teamxmlexport/class_team_export.php'),
     'filesLoadTime' => array()
 ));
@@ -83,8 +83,8 @@ Module::register(array(
     'modname'       => 'memmatches',
     'author'        => 'Nicholas Mossor Rathmann',
     'date'          => '2008-2009',
-    'setCanvas'     => true,
-    'main'          => 'Memmatches::main',
+    'setCanvas'     => true, 
+    'main'          => 'Memmatches::main', 
     'filesRunTime'  => array('memmatches/class_memmatches.php'),
     'filesLoadTime' => array()
 ));
@@ -93,8 +93,8 @@ Module::register(array(
     'modname'       => 'comparison',
     'author'        => 'Nicholas Mossor Rathmann',
     'date'          => '2009',
-    'setCanvas'     => true,
-    'main'          => 'Comparison::main',
+    'setCanvas'     => true, 
+    'main'          => 'Comparison::main', 
     'filesRunTime'  => array('comparison/class_comparison.php'),
     'filesLoadTime' => array()
 ));
@@ -103,8 +103,8 @@ Module::register(array(
     'modname'       => 'leegmgr',
     'author'        => 'William Leonard',
     'date'          => '2009',
-    'setCanvas'     => true,
-    'main'          => 'leegmgr_load',
+    'setCanvas'     => true, 
+    'main'          => 'leegmgr_load', 
     'filesRunTime'  => array('leegmgr/class_match_botocs.php', 'leegmgr/uploadinc.php'),
     'filesLoadTime' => array('leegmgr/settings.php')
 ));
@@ -113,9 +113,9 @@ Module::register(array(
     'modname'       => 'botocsxml',
     'author'        => 'William Leonard',
     'date'          => '2009',
-    'setCanvas'     => false,
-    'main'          => 'botocsxml_load',
-    'filesRunTime'  => array('leegmgr/xml_roster.php'),
+    'setCanvas'     => false, 
+    'main'          => 'XML_BOTOCS::main', 
+    'filesRunTime'  => array('leegmgr/class_xml_botocs.php'),
     'filesLoadTime' => array('leegmgr/settings.php')
 ));
 
@@ -123,8 +123,8 @@ Module::register(array(
     'modname'       => 'registration',
     'author'        => 'William Leonard',
     'date'          => '2009',
-    'setCanvas'     => true,
-    'main'          => 'Registration::main',
+    'setCanvas'     => true, 
+    'main'          => 'Registration::main', 
     'filesRunTime'  => array('registration/class_registration.php'),
     'filesLoadTime' => array('registration/settings.php')
 ));
@@ -149,6 +149,5 @@ Module::register(array(
     'filesRunTime'  => array('cyanide/class_CyanideTeam.php', 'cyanide/team_import.php'),
     'filesLoadTime' => array('cyanide/settings.php')
 ));
-
 
 ?>
