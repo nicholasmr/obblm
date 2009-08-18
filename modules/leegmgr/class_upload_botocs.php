1702d4aa3471a0e255917ad87516956612c0dfd2
funnyfingers <funnyfingers@hotmail.com>
2009-08-18 21:44:31 +0000

    leegmgr: oops, fixed the form submittal to leegmgr from leegmgr1 that was used for testing.

--- modules/leegmgr/class_upload_botocs.php
+++ modules/leegmgr/class_upload_botocs.php
@@ -435,7 +435,7 @@ class UPLOAD_BOTOCS
 
         $form = "
             <!-- The data encoding type, enctype, MUST be specified as below -->
-            <form enctype='multipart/form-data' action='handler.php?type=leegmgr1' method='POST'>
+            <form enctype='multipart/form-data' action='handler.php?type=leegmgr' method='POST'>
                 <!-- MAX_FILE_SIZE must precede the file input field -->
                 <input type='hidden' name='MAX_FILE_SIZE' value='100000' />
                 <!-- Name of input element determines name in $_FILES array -->
