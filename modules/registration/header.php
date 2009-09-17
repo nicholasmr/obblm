2896e331ed9125521ea1ebacf5ad77e4f233287c
funnyfingers <funnyfingers@hotmail.com>
2009-09-17 23:30:06 +0000

    registration: Issue 248 and fixed a bug that showed up when there was an error like invalid user name.

--- modules/registration/header.php
+++ modules/registration/header.php
@@ -14,6 +14,11 @@ define('ACCESS_LEVEL', 2);
 define('USERNAME_ERROR', 'The username already exists or must be at least 3 characters long.');
 define('PASSWORD_ERROR', 'The password must be at least 5 characters long');
 define('EMAIL_ERROR', 'The email address is not valid.');
+define('EMAIL_SUBJECT', 'New user registration');
+define('EMAIL_MESSAGE', 'You have received a new registration.  To activate the user you must un-retire the coach using the admin page.
 
+coach: ');
+define('SEND_EMAIL_ERROR', 'Registration was successful, but the administrators were not notified via email.  It may take longer to activate your account.');
+define('SUCCESS_MSG', 'Registration was successful.  A site administrator will enable your account or contact you for verification.');
 define('USERNAME_RESET_ERROR', 'The username does not exist.');
 ?>
