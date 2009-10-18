0b16f3caeb1c0762cc4b0b610c198725768a5fae
Nimda562 <Nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2009-10-18 16:34:02 +0000

    Small revision of Will's upload botocs SQL upgrade code.

--- modules/leegmgr/class_upload_botocs.php
+++ modules/leegmgr/class_upload_botocs.php
@@ -795,13 +795,14 @@ class UPLOAD_BOTOCS implements ModuleInterface
                     hash    VARCHAR(32)
                 )',
                 // In case of people having used the 0.80 revisions we must save their exisitng leegmgr data:
+                'DROP TABLE IF EXISTS leegmgr_matches_temp',
                 'CREATE TABLE leegmgr_matches_temp (
                     mid     MEDIUMINT,
                     replay  MEDIUMBLOB,
                     hash    VARCHAR(32)
                 )
                 ',
-                'INSERT INTO leegmgr_matches_temp (mid, hash) SELECT match_id, hash_botocs FROM matches',
+                SQLUpgrade::runIfColumnExists('matches', 'hash_botocs', 'INSERT INTO leegmgr_matches_temp (mid, hash) SELECT match_id, hash_botocs FROM matches'),
                 'UPDATE leegmgr_matches_temp, leegmgr_matches SET leegmgr_matches_temp.replay = leegmgr_matches.replay WHERE leegmgr_matches_temp.mid = leegmgr_matches.mid',
                 'DROP TABLE leegmgr_matches',
                 'ALTER TABLE leegmgr_matches_temp RENAME TO leegmgr_matches',
