df0cebbde0689f89d9d2a0f182bc67bac39515ac
funnyfingers <funnyfingers@hotmail.com>
2009-12-30 01:51:37 +0000

    added error output from Match::Create and allow settings to come from the league.

--- modules/leegmgr/settings.php
+++ modules/leegmgr/settings.php
@@ -1,12 +1,16 @@
 <?php
+#The isset() check allows leagues to choose their own leegmgr settings.
 
 global $settings;
 
-$settings['leegmgr_enabled']  = true;  // Enables upload of BOTOCS LRB5 application match reports.
-$settings['leegmgr_schedule'] = true; // Uploads report to a scheduled match.  The options are [false|true|"strict"]
-                                       // false does not check for scheduled matches
-                                       // true checks for scheduled matches and will create a match if not found
-                                       // "strict" will allow only scheduled matches to be used
-$settings['leegmgr_extrastats'] = true; // Enables the reporting of extra stats and the use of the alternate XSD file.
+if ( !isset($settings['leegmgr_enabled']) )
+    $settings['leegmgr_enabled']  = true;   // Enables upload of BOTOCS LRB5 application match reports.
+if ( !isset($settings['leegmgr_schedule']) )
+    $settings['leegmgr_schedule'] = true;   // Uploads report to a scheduled match.  The options are [false|true|"strict"]
+                                            // false does not check for scheduled matches
+                                            // true checks for scheduled matches and will create a match if not found
+                                            // "strict" will allow only scheduled matches to be used
+if ( !isset($settings['leegmgr_extrastats']) )
+    $settings['leegmgr_extrastats'] = true; // Enables the reporting of extra stats and the use of the alternate XSD file.
 
 ?>
