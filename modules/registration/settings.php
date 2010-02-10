2393b8915030e5b05a563929885bdda236c766de
funnyfingers <funnyfingers@hotmail.com>
2010-02-10 00:31:07 +0000

    registration:
    Update issue 354
    Status: Fixed
    Fixed and thoroughly tested.

--- modules/registration/settings.php
+++ modules/registration/settings.php
@@ -2,4 +2,5 @@
 global $settings;
 $settings['allow_registration']      = true;                   // Default is true.
 $settings['registration_webmaster'] = "webmaster@example.com"; // Default is "webmaster@example.com".
+$settings['lang'] = 'en-GB';
 ?>
