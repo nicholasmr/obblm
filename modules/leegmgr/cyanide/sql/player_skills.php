9f0c1eacc0e28cffaf975b9b1466f3e1f06aeafc
funnyfingers <funnyfingers@hotmail.com>
2010-02-19 00:57:47 +0000

    adding cyanide libraries from MasterSage.

---
+++ modules/leegmgr/cyanide/sql/player_skills.php
@@ -0,0 +1,9 @@
+<?php
+
+$_t_player_skills = 
+'
+DROP TABLE IF EXISTS "Player_Skills";
+CREATE TABLE Player_Skills (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,idPlayer_Listing INTEGER ,idSkill_Listing INTEGER );
+';
+
+?>
