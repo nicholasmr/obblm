187270c91f08734c81f899295ef8210c09c90f0c
williams2931 <williams2931@comcast.net>
2010-02-20 02:48:16 +0000

    <MODULE>: <BRIEF>
    Update issue <336>
    Status: Accepted/Started/Postponed/Fixed/Verified/Invalid/Duplicate/Wontfix
    <COMMENT>
    
    Added GNU lic info to the top of the class... also testing svn client

--- modules/leegmgr/cyanide/lib_cy_team_db.php
+++ modules/leegmgr/cyanide/lib_cy_team_db.php
@@ -1,10 +1,25 @@
 <?php
 
-//examples
-//comment this out for production
-
-//End Example
-
+/*
+ *  Copyright (c) Ryan Williams <email protected> 2010. All Rights Reserved.
+ *
+ *
+ *  This file is part of OBBLM.
+ *
+ *  OBBLM is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OBBLM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 class cy_team_db {
 	var $_db_con;
