e4662e810c291fd62ffa1ebc885f1b4c34bf4a11
nicholas.rathmann <nicholas.rathmann@6946552a-eee9-11dd-8501-3b2f46a228df>
2010-02-23 22:22:50 +0000

    Cleaned up local settings file. Removed the requirement of mv_ prefixes for fields. Added right col. boxes display order option. Added custom titles for tournament stadings boxes. Implemented issue 365.

--- localsettings/settings_1.php
+++ localsettings/settings_1.php
@@ -7,13 +7,13 @@
 $settings['league_name'] = 'Placeholder title for league with ID = 1'; // Name of the site or the league name if only one league is being managed.
 $settings['forum_url'] = 'http://localhost'; // URL of league forum, if you have such. If not then leave this empty, that is = '' (two quotes only).
 $settings['stylesheet'] = 1;                 // Default is 1. OBBLM CSS stylesheet for non-logged in guests. Currently stylesheet 1 and 2 are the only existing stylesheets.
-$settings['lang'] = 'en-GB';                 // Deafult language. Existing: en-GB, es.
+$settings['lang'] = 'en-GB';                 // Deafult language. Existing: en-GB, es, de.
 $settings['welcome'] = 'Please replace this line in your local league settings file, <i>localsettings/settings_1.php</i>, with your own league greeting message for the league with ID = 1';
 $settings['rules'] = 'Please replace this line in your local league settings file, <i>localsettings/settings_1.php</i>, with your own league rules description for the league with ID = 1';
 
 $settings['entries'] = array(
-    'messageboard'      => 5,   // Number of entries on the main page messageboard.
-    'latestgames'       => 5,   // Number of entries in the main page table "latest games".
+    'messageboard'      => 5,   // Number of entries on the front page (FP) messageboard.
+    'latestgames'       => 5,   // Number of entries on the front page (FP) table "latest games".
     'standings_players' => 30,  // Number of entries on the general players stadings table.
     'standings_teams'   => 30,  // Number of entries on the general teams   stadings table.
     'standings_coaches' => 30,  // Number of entries on the general coaches stadings table.
@@ -21,14 +21,12 @@ $settings['entries'] = array(
 
 // Front Page (FP) tournament standings boxes
 $settings['fp_standings'] = array(
-    # This would display a standings box of the top 6 teams in tournament with ID = 1.
+    # This will display a standings box of the top 6 teams in tournament with ID = 1.
     1 => array(
+        'title'  => 'Tournament 1 standings',
         'length' => 6,
-        'fields' => array(
-            # Displayed table column name => OBBLM field name.
-            'Name' => 'name', 'PTS' => 'mv_pts', 'CAS' => 'mv_cas', 
-            'W' => 'mv_won', 'L' => 'mv_lost', 'D' => 'mv_draw', 'GF' => 'mv_gf', 'GA' => 'mv_ga'
-        ),
+        # Format: "Displayed table column name" => "OBBLM field name".
+        'fields' => array('Name' => 'name', 'PTS' => 'pts', 'CAS' => 'cas', 'W' => 'won', 'L' => 'lost', 'D' => 'draw', 'GF' => 'gf', 'GA' => 'ga'),
     ),
 );
 
@@ -36,14 +34,20 @@ $settings['fp_standings'] = array(
 $settings['fp_leaders'] = array(
     /* 
         Note, you can NOT make expressions out of leader fields e.g.:
-            'mv_cas+mv_td' => array('title' => 'Most CAS+TD', 'length' => 5)
+            'cas+td' => array('title' => 'Most CAS+TD', 'length' => 5)
     */
     
-    'mv_cas' => array('title' => 'Most casualties',  'length' => 5),
-#    'mv_td'  => array('title' => 'Most touchdowns',  'length' => 5),
-#    'mv_cp'  => array('title' => 'Most completions', 'length' => 5),
-#    'mv_ki'  => array('title' => 'Most killed',      'length' => 5),
+    # This will display the below player leaders boxes for the tournament with ID = 1.
+    1 => array(
+        'cas' => array('title' => 'Tournament 1 most casualties',  'length' => 5),
+        'td'  => array('title' => 'Tournament 1 most touchdowns',  'length' => 5),
+        'cp'  => array('title' => 'Tournament 1 most completions', 'length' => 5),
+        'ki'  => array('title' => 'Tournament 1 most killed',      'length' => 5),
+    )
 );
+
+// This is the order in which the right side boxes of the front page are displayed.
+$settings['fp_boxes_order'] = array('standings', 'latestgames', 'leaders');
 
 $settings['show_sold_journeymen']  = true;  // Default is true. Show sold journeymen on rosters in detailed view mode.
 $settings['show_stars_mercs']      = true;  // Default is true. Show summed up stats for earlier hired star players and mercenaries on rosters in detailed view mode.
