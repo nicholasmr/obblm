e12e15148994ed2a60811aaecaf60db22e6afd0b
williams2931@comcast.net <williams2931@comcast.net@6946552a-eee9-11dd-8501-3b2f46a228df>
2010-03-03 05:36:20 +0000

    <MODULE>: <BRIEF>
    Update issue <BUGID>
    Status: Accepted/Started/Postponed/Fixed/Verified/Invalid/Duplicate/Wontfix
    <COMMENT>
    
    Corrected type-o in $awateam -> $awayteam

--- trunk/modules/leegmgr/cyanide/lib_cy_match_db.php
+++ trunk/modules/leegmgr/cyanide/lib_cy_match_db.php
@@ -259,7 +259,7 @@ class cy_match_db {
 	}
 	private function set_home_winnings() {
 		$d1 = rand(1,6);
-		$this->homewinnings = $d1 + ($this->homefame * 10000) +  (( $this->winner != $this->awateam ) ? 10000 : 0);
+		$this->homewinnings = $d1 + ($this->homefame * 10000) +  (( $this->winner != $this->awayteam ) ? 10000 : 0);
 	}
 	private function set_away_winnings() {
 		$d1 = rand(1,6);
