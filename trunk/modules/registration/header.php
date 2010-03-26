120e6be9552491b31dec0786140bd6e284703572
funnyfingers@hotmail.com <funnyfingers@hotmail.com@6946552a-eee9-11dd-8501-3b2f46a228df>
2010-03-26 22:56:13 +0000

    registration: Added activation form.

--- trunk/modules/registration/header.php
+++ trunk/modules/registration/header.php
@@ -2,10 +2,11 @@
 // User table information
 define('USERTABLE', 'coaches');
 define('USERNAME', 'name');
+define('USERID', 'coach_id');
 define('PASSWORD', 'passwd');
 define('EMAIL', 'mail');
 define('ACTIVATION', 'retired');
-define('NOT_ACTIVATED', 1);
+define('NOT_ACTIVATED', 2);
 define('IS_ACTIVATED', 0);
 define('ACCESS', 'ring');
 define('ACCESS_LEVEL', Coach::T_RING_GLOBAL_NONE);
@@ -15,10 +16,19 @@ define('USERNAME_ERROR', 'The username already exists or must be at least 3 char
 define('PASSWORD_ERROR', 'The password must be at least 5 characters long');
 define('EMAIL_ERROR', 'The email address is not valid.');
 define('EMAIL_SUBJECT', 'New user registration');
-define('EMAIL_MESSAGE', 'You have received a new registration.  To activate the user you must un-retire the coach using the admin page.
+define('EMAIL_MESSAGE', 'You have received a new registration.  To activate the user, use the activation page by visiting the link below.
 
 coach: ');
+
+define('EMAIL_SUBJECT_ACTIVATED', 'Your coach has been activated.');
+//The coach name is printed before the following message
+define('EMAIL_MESSAGE_ACTIVATED', ': your coach on the site has been activated.
+
+Website: ');
+
 define('SEND_EMAIL_ERROR', 'Registration was successful, but the administrators were not notified via email.  It may take longer to activate your account.');
+define('SEND_EMAIL_ERROR_ACTIVATED', 'Activation was successful, but the user was not notified by email.');
 define('SUCCESS_MSG', 'Registration was successful.  A site administrator will enable your account or contact you for verification.');
 define('USERNAME_RESET_ERROR', 'The username does not exist.');
+define('ACTIVATION_NOT_NEEDED', "The username specified does not need to be activated.")
 ?>
