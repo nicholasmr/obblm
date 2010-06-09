8b6ddba0f79273d48cf78bfe810c269ba613fe10
nicholas.rathmann <nicholas.rathmann@6946552a-eee9-11dd-8501-3b2f46a228df>
2010-06-09 12:58:02 +0000

    issue 426 fixed

--- trunk/modules/wanted/class_wanted.php
+++ trunk/modules/wanted/class_wanted.php
@@ -294,7 +294,7 @@ public static function makeList($ALLOW_EDIT)
                         ?>
                         </td>
                         <td align="right" style="width: 30%;">
-                            <img border='0px' height='100' width='100' alt='player picture' src='<?php echo NO_PIC;?>'>
+                            <img border='0px' height='100' width='100' alt='player picture' src="<?php $img = new ImageSubSys(T_OBJ_PLAYER, $p->player_id); echo $img->getPath();?>">
                         </td>
                     </tr>
                     <tr>
