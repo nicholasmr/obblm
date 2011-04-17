57f92623c7e48bb6de58e962999947c24831ef09
nimda562 <nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2011-04-17 22:45:06 +0000

    removed old modules settings files

--- trunk/modules/registration/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $settings;
-$settings['allow_registration']      = true;                   // Default is true.
-$settings['registration_webmaster'] = "webmaster@example.com"; // Default is "webmaster@example.com".
-$settings['lang'] = 'en-GB';
-?>
