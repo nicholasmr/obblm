832b176dae18f53f7fff76196b3b31929c0c41ef
nimda562 <nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2011-04-17 22:07:12 +0000

    Common modules settings file created (not finished).

---
+++ trunk/settings_modules.php
@@ -0,0 +1,29 @@
+<?php
+
+/*****************
+ * Enable/disable modules
+ *****************/
+ 
+// Change value from true to false if you wish to disable a module.
+
+$settings['modules_enabled'] = array(
+    'IndcPage'           => true, # Inducements try-out
+    'PDFroster'          => true, # Team PDF roster
+    'RSSfeed'            => true, # Site RSS feed
+    'SGraph'             => true, # Graphical statistics
+    'Memmatches'         => true, # Memorable matches viewer
+    'Wanted'             => true, # Wanted list
+    'HOF'                => true, # Hall of fame
+    'Prize'              => true, # Tournament prizes list
+    'UPLOAD_BOTOCS'      => true, # Allow upload of a BOTOCS match
+    'XML_BOTOCS'         => true, # BOTOCS XML export of team
+    'Registration'       => true, # Allows users to register on the site.
+    'Search'             => true, # Search for coaches and teams.
+    'PDFMatchReport'     => true, # Generating PDF forms for tabletop match reports.
+);
+
+/*****************
+ * Module settings
+ *****************/
+
+?>
