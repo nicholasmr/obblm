c5fee75bd7787666f9b36e2ce881f681d2d3a258
nimda562 <nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2011-06-20 11:23:41 +0000

    Fixed issue 538. Updated gallery module.

--- modules/gallery/class_gallery.php
+++ modules/gallery/class_gallery.php
@@ -9,6 +9,7 @@ public static function main($argv) # argv = argument vector (array).
     global $lng;
 
     title($lng->getTrn('name', 'Gallery'));
+    list($sel_lid, $HTML_LeagueSelector) = HTMLOUT::simpleLeagueSelector();
 
     if (isset($_POST['type'])) {
         echo "<center><a href='handler.php?type=gallery'>".$lng->getTrn('common/back')."</a></center>\n";
@@ -26,7 +27,9 @@ public static function main($argv) # argv = argument vector (array).
 
             case 'stad':
                 echo "<b>".$lng->getTrn('stads', 'Gallery')."</b><br><hr><br>\n";
-                foreach (Team::getTeams() as $t) {
+                $teams = get_rows('teams', array('team_id', 'name'), array("f_lid = $sel_lid"));
+                objsort($teams, array('+name'));
+                foreach ($teams as $t) {
                     $img = new ImageSubSys(IMGTYPE_TEAMSTADIUM, $t->team_id);
                     $pic = $img->getPath();
                     echo "<div style='float:left; padding:10px;'>$t->name<br><a href='$pic'><img HEIGHT=150 src='$pic' alt='pic'></a></div>";
@@ -35,7 +38,9 @@ public static function main($argv) # argv = argument vector (array).
 
             case 'coach':
                 echo "<b>".$lng->getTrn('coaches', 'Gallery')."</b><br><hr><br>\n";
-                foreach (Coach::getCoaches() as $c) {
+                $q = "SELECT coach_id, name FROM coaches,memberships WHERE cid = coach_id AND lid = $sel_lid GROUP BY cid, lid ORDER BY name ASC";
+                $result = mysql_query($q);
+                while ($c = mysql_fetch_object($result)) {
                     $img = new ImageSubSys(IMGTYPE_COACH, $c->coach_id);
                     $pic = $img->getPath();
                     echo "<div style='float:left; padding:10px;'>$c->name<br><a href='$pic'><img HEIGHT=150 src='$pic' alt='pic'></a></div>";
@@ -45,17 +50,19 @@ public static function main($argv) # argv = argument vector (array).
 
         return;
     }
-
+    
+    echo $HTML_LeagueSelector;
+    echo "<br><br>\n";
     echo $lng->getTrn('note', 'Gallery');
     ?>
     <ul>
     <li>
         <?php 
-        $teams = get_rows('teams', array('team_id', 'name'));
+        $teams = get_rows('teams', array('team_id', 'name'), array("f_lid = $sel_lid"));
         objsort($teams, array('+name'));
         echo $lng->getTrn('players', 'Gallery')."
         <form method='POST' style='display:inline; margin:0px;'><select name='tid' onChange='this.form.submit();'>
-        <option value='0'>-".$lng->getTrn('none', 'Gallery')."-</option>".
+        <option value='0'>&mdash; ".$lng->getTrn('none', 'Gallery')." &mdash;</option>".
         implode("\n", array_map(create_function('$o', 'return "<option value=\'$o->team_id\'>$o->name</option>";'), $teams))
         ."</select><input type='hidden' name='type' value='team'></form>
         ";?>
