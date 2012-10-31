3f9309b477417db997147a9c80d8caff0d65fe9b
nimda562 <nimda562@6946552a-eee9-11dd-8501-3b2f46a228df>
2012-10-31 09:33:28 +0000

    Made core panel more readable and friendly

--- admin/admin_cpanel.php
+++ admin/admin_cpanel.php
@@ -12,7 +12,7 @@ if (isset($_POST['act'])) {
         switch ($act) 
         {
             case 'gdsync': status(SQLCore::syncGameData(), 'PHP game data synced with DB'); break;
-            case 'tblidx': status(SQLCore::installTableIndexes(), 'Indexes installed'); break;
+            case 'tblidx': status(SQLCore::installTableIndexes(), 'Indices installed'); break;
             case 'funcs': status(SQLCore::installProcsAndFuncs(true), 'DB functions and procedures installed'); break;
         }
     }
@@ -20,23 +20,26 @@ if (isset($_POST['act'])) {
 title($lng->getTrn('menu/admin_menu/cpanel'));
 ?>
 <p>
-This page allows access to OBBLM back-end maintenance and synchronisation routines.
+OBBLM database maintenance and synchronisation routines.<br><br>
 </p>
 <form method="POST">
-    <b>DB synchronisation procedures:</b><br>
-    <INPUT TYPE=RADIO NAME="act" VALUE="syncAll()"><i>syncAll()</i> - Synchronises all stats, relations and dynamic properties - may take a few minutes!<br>
+    <b>Database maintenance:</b><br>
+    <INPUT TYPE=RADIO NAME="act" VALUE="gdsync">Synchronise database with game data files. &mdash; Run this when having changed the PHP game data files <i>lib/game_data*.php</i>.<br>
+    <INPUT TYPE=RADIO NAME="act" VALUE="funcs">Re-install database back-end procedures and functions. &mdash; Run this when having altered the "house ranking system" definitions in <i>settings.php</i>.<br>
+    <INPUT TYPE=RADIO NAME="act" VALUE="tblidx">Re-install table indices.<br>
+    <br>
+    <b>Database synchronisation:</b><br>
+    <INPUT TYPE=RADIO NAME="act" VALUE="syncAll()"><i>syncAll()</i> &mdash; Synchronises all stats, relations and dynamic properties. This may take a few minutes!<br>
     <!--
     <INPUT TYPE=RADIO NAME="act" VALUE="syncAllMVs()"><i>syncAllMVs()</i> - Synchronises all stats.<br>
     <INPUT TYPE=RADIO NAME="act" VALUE="syncAllDProps()"><i>syncAllDProps()</i> - Synchronises all dynamic properties (TVs, PVs etc.).<br>
     <INPUT TYPE=RADIO NAME="act" VALUE="syncAllRels()"><i>syncAllRels()</i> - Synchronises all object (player, team, coach) ownership relations.<br>
     -->
-    <br><b>DB maitenance:</b><br>
-    <INPUT TYPE=RADIO NAME="act" VALUE="gdsync">Synchronise the PHP-stored BB game data (<i>lib/game_data*.php</i> files) with DB. <u>Run this when</u> having changed the BB game data files.<br>
-    <INPUT TYPE=RADIO NAME="act" VALUE="funcs">Re-install DB back-end procedures and functions. <u>Run this when</u> having altered the "house ranking systems" rule definitions defined in <i>settings.php</i>.<br>
-    <INPUT TYPE=RADIO NAME="act" VALUE="tblidx">Re-install table indexes.<br>
-    <br>
-    <input type="submit" name='submit' value="Run">
+    <br><br>
+    <input type="submit" name='submit' value="Run it">
 </form>
+<br>
+<hr width='30%' align='left'>
 
 <?php
 ?>
