4dd5d4b7bed012ec68deaad4ab172be90a50f31f
rythos42 <rythos42@gmail.com>
2015-09-29 16:57:11 -0700

    Translations

--- lib/class_mobile_htmlout.php
+++ lib/class_mobile_htmlout.php
@@ -1,7 +1,7 @@
 <?php
 class Mobile_HTMLOUT {
     public static function sec_mobile_main() {
-        global $coach;
+        global $coach, $lng;
         
         $teams = $coach->getTeams();
         $selectedTeamId = isset($_POST["SelectedTeam"]) ? $_POST["SelectedTeam"] : $teams[0]->team_id;
@@ -45,12 +45,12 @@ class Mobile_HTMLOUT {
                     ?>
                 </select>
                 <span class="button-panel">
-                    <a href="<?php echo getFormAction() . '?logout=1'; ?>">Logout</a>
+                    <a href="<?php echo getFormAction() . '?logout=1'; ?>"><?php echo $lng->getTrn('menu/logout'); ?></a>
                 </span>
                 <div id="tabs">
                     <ul>
-                        <li><a href="#Teams">Teams</a></li>
-                        <li><a href="#Games">Games</a></li>
+                        <li><a href="#Teams"><?php echo $lng->getTrn('common/teams'); ?></a></li>
+                        <li><a href="#Games"><?php echo $lng->getTrn('menu/matches_menu/name'); ?></a></li>
                     </ul>
                     <?php Mobile_HTMLOUT::teamSummaryView($playersOnSelectedTeam); ?>
                     <?php Mobile_HTMLOUT::matchSummaryView($allMatches); ?>
@@ -61,16 +61,17 @@ class Mobile_HTMLOUT {
     }
     
     private static function teamSummaryView($playersOnSelectedTeam) {
+        global $lng;
         ?>
         <div id="Teams">
             <table id="Players">
                 <thead>
                     <tr>
                         <th></th>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Stats</th>
-                        <th>Skills</th>
+                        <th><?php echo $lng->getTrn('common/name'); ?></th>
+                        <th><?php echo $lng->getTrn('common/pos'); ?></th>
+                        <th><?php echo $lng->getTrn('common/stats'); ?></th>
+                        <th><?php echo $lng->getTrn('common/skills'); ?></th>
                         <th>SPP</th>
                     </tr>
                 </thead>
@@ -90,11 +91,11 @@ class Mobile_HTMLOUT {
             <div id="PlayerDialog" data-bind="with: playerDialogViewModel">
                 <table>
                     <tbody>
-                        <tr><td>Number:</td><td class="data" data-bind="text: number"></td></tr>
-                        <tr><td>Player name:</td><td class="data" data-bind="text: name"></td></tr>
-                        <tr><td>Position:</td><td class="data" data-bind="text: position"></td></tr>
+                        <tr><td><?php echo $lng->getTrn('common/number'); ?>:</td><td class="data" data-bind="text: number"></td></tr>
+                        <tr><td><?php echo $lng->getTrn('common/name'); ?>:</td><td class="data" data-bind="text: name"></td></tr>
+                        <tr><td><?php echo $lng->getTrn('common/pos'); ?>:</td><td class="data" data-bind="text: position"></td></tr>
                         <tr><td>MA/ST/AG/AV:</td><td class="data" data-bind="text: statString"></td></tr>
-                        <tr><td>Skills:</td><td class="data" data-bind="html: skillsString"></td></tr>
+                        <tr><td><?php echo $lng->getTrn('common/skills'); ?>:</td><td class="data" data-bind="html: skillsString"></td></tr>
                         <tr><td>SPP:</td><td class="data" data-bind="text: spp"></td></tr>
                     </tbody>
                 </table>
@@ -104,9 +105,10 @@ class Mobile_HTMLOUT {
     }
     
     private static function matchSummaryView($allMatches) {
+        global $lng;
         ?>
         <div id="Games">
-            <div>Recent:</div>
+            <div><?php echo $lng->getTrn('profile/team/games'); ?></div>
             <table>
                 <tbody>
                 <?php
@@ -137,57 +139,58 @@ class Mobile_HTMLOUT {
     }
     
     private static function readonlyMatchView() {
+        global $lng;
         ?>
         <div>
-            <span class="label">Team:</span>
+            <span class="label"><?php echo $lng->getTrn('common/team'); ?>:</span>
             <span class="data" data-bind="text: name"></span>
         </div>
         <div>
-            <span class="label">Score:</span>
+            <span class="label"><?php echo $lng->getTrn('common/score'); ?>:</span>
             <span class="data" data-bind="text: myScore"></span> for me, to <span class="data" data-bind="text: theirScore"></span>
         </div>
         <div>
-            <span class="label">Treasury:</span>
+            <span class="label"><?php echo $lng->getTrn('matches/report/treas'); ?>:</span>
             <span class="data" data-bind="text: treasuryChange"></span>
         </div>
         <div>
-            <span class="label">Fan Factor:</span>
+            <span class="label"><?php echo $lng->getTrn('matches/report/ff'); ?>:</span>
             <span class="data" data-bind="text: fanFactorChange"></span>
         </div>
         <div id="SelectedPlayer">
-            <span>Player:</span>
+            <span><?php echo $lng->getTrn('common/player'); ?>:</span>
             <select data-bind="value: selectedPlayer, options: playersOnTeam, optionsText: 'name'"></select>
             <div data-bind="with: selectedPlayerViewModel">
                 <div>
-                    <span class="label">MVP:</span>
+                    <span class="label"><?php echo $lng->getTrn('matches/report/mvp'); ?>:</span>
                     <span class="data" data-bind="text: mvp"></span>
                 </div>
                 <div>
-                    <span class="label">Completions:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/cp'); ?>:</span>
                     <span class="data" data-bind="text: completions"></span>
                 </div>
                 <div>
-                    <span class="label">Touchdowns:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/td'); ?>:</span>
                     <span class="data" data-bind="text: touchdowns"></span>
                 </div>
                 <div>
-                    <span class="label">Interceptions:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/intcpt'); ?>:</span>
                     <span class="data" data-bind="text: interceptions"></span>
                 </div>
                 <div>
-                    <span class="label">Badly hurt:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/bh'); ?>:</span>
                     <span class="data" data-bind="text: badlyHurt"></span>
                 </div>
                 <div>
-                    <span class="label">Sustained injury:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/si'); ?>:</span>
                     <span class="data" data-bind="text: sustainedInjury"></span>
                 </div>
                 <div>
-                    <span class="label">Killed:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/ki'); ?>:</span>
                     <span class="data" data-bind="text: killed"></span>
                 </div>
                 <div>
-                    <span class="label">Injured:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/injs'); ?>:</span>
                     <span class="data" data-bind="text: injured"></span>
                 </div>
             </div>
@@ -196,68 +199,69 @@ class Mobile_HTMLOUT {
     }
 
     private static function editableMatchView() {
+        global $lng;
         ?>
         <div class="row">
-            <span class="label">Team:</span>
+            <span class="label"><?php echo $lng->getTrn('common/team'); ?>:</span>
             <span class="data" data-bind="text: name"></span>
         </div>
         <div class="row">
-            <span class="label">Score:</span>
+            <span class="label"><?php echo $lng->getTrn('common/score'); ?>:</span>
             <input type="number" data-bind="value: myScore" /> for me, to <input type="number" data-bind="value: theirScore" />
         </div>
         <div class="row">
-            <span class="label">Treasury:</span>
+            <span class="label"><?php echo $lng->getTrn('matches/report/treas'); ?>:</span>
             <input type="number" data-bind="value: treasuryChange" />k
         </div>
         <div class="treasury-change-field row">
-            <span class="label">Fan Factor:</span>
+            <span class="label"><?php echo $lng->getTrn('matches/report/ff'); ?>:</span>
             <span>1<input type="radio" name="TreasuryChange" data-bind="checked: fanFactorChange" value="1" /></span>
             <span>0<input type="radio" name="TreasuryChange" data-bind="checked: fanFactorChange" value="0" /></span>
             <span>-1<input type="radio" name="TreasuryChange" data-bind="checked: fanFactorChange" value="-1" /></span>
         </div>
         <div id="SelectedPlayer">
-            <span>Player:</span>
+            <span><?php echo $lng->getTrn('common/player'); ?>:</span>
             <select data-bind="value: selectedPlayer, options: playersOnTeam, optionsText: 'name'"></select>
             <div data-bind="with: selectedPlayerViewModel">
                 <div class="row">
-                    <span class="label">MVP:</span>
+                    <span class="label"><?php echo $lng->getTrn('matches/report/mvp'); ?>:</span>
                     <span>0<input type="radio" name="Mvp" data-bind="checked: mvp" value="0" /></span>
                     <span>1<input type="radio" name="Mvp" data-bind="checked: mvp" value="1" /></span>
                     <span>2<input type="radio" name="Mvp" data-bind="checked: mvp" value="2" /></span>
                 </div>
                 <div class="row">
-                    <span class="label">Completions:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/cp'); ?>:</span>
                     <input type="number" data-bind="value: completions" />
                 </div>
                 <div class="row">
-                    <span class="label">Touchdowns:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/td'); ?>:</span>
                     <input type="number" data-bind="value: touchdowns" />
                 </div>
                 <div class="row">
-                    <span class="label">Interceptions:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/intcpt'); ?>:</span>
                     <input type="number" data-bind="value: interceptions" />
                 </div>
                 <div class="row">
-                    <span class="label">Badly hurt:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/bh'); ?>:</span>
                     <input type="number" data-bind="value: badlyHurt" />
                 </div>
                 <div class="row">
-                    <span class="label">Sustained injury:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/si'); ?>:</span>
                     <input type="number" data-bind="value: sustainedInjury" />
                 </div>
                 <div class="row">
-                    <span class="label">Killed:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/ki'); ?>:</span>
                     <input type="number" data-bind="value: killed" />
                 </div>
                 <div class="row">
-                    <span class="label">Injured:</span>
+                    <span class="label"><?php echo $lng->getTrn('common/injs'); ?>:</span>
                     <span class="data" data-bind="text: injured"></span>
                 </div>
             </div>
         </div>
         <div class="button-panel">
-            <input type="button" value="Save" data-bind="click: saveMatch" />
-            <a href="#" data-bind="click: close">Close</a>
+            <input type="button" value="<?php echo $lng->getTrn('common/save'); ?>" data-bind="click: saveMatch" />
+            <a href="#" data-bind="click: close"><?php echo $lng->getTrn('common/back'); ?></a>
         </div>
         <?php
     }
