c15a3c3e7e7269709849192b4a576fc86638e855
rythos42 <Blyrble7>
2015-12-14 10:17:51 -0800

    Making "open" links a little larger. Displaying treasury and FF alongside TV.

--- lib/class_mobile_htmlout.php
+++ lib/class_mobile_htmlout.php
@@ -77,7 +77,7 @@ class Mobile_HTMLOUT {
                     ?>
                 </select>
                 <span>
-                    TV<?php echo $selectedTeam->tv/1000; ?>
+                    <?php echo 'TV' . $selectedTeam->tv/1000 . ', ' . $selectedTeam->treasury/1000 . 'k, FF' . $selectedTeam->rg_ff; ?> 
                 </span>
                 <span class="button-panel">
 					<img id="open-menu" src="images/menu.svg" alt="Menu" class="icon ui-button ui-state-default ui-corner-all" data-bind="click: showMenu" />
