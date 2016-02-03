dae0ee96baf3e48fa3ba56ed71ef9f5b7ff3f917
rythos42 <Blyrble7>
2016-02-03 10:25:08 -0800

    Adding travis support for auto-deploy to thenafdev

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/* jshint node: true */
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'thenafdev.obblm.com',
+					port: 21,
+					authKey: 'key1'
+				},
+				src: '.',
+				dest: 'public_html',
+				exclusions: ['.gitignore', '.travis.yml', 'Gruntfile.js', 'notes.txt', 'README.md', 'package.json', '.ftppass', 'LICENSE', '.git', 'node_modules', 'test']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
+	grunt.registerTask('test', []);
+	grunt.registerTask('deploy', ['create-ftp-file', 'ftp-deploy']);
+	
+	grunt.registerTask('create-ftp-file', 'Create an authentication file for FTP', function() {
+		var ftpUsername = grunt.option('ftpUsername'),
+			ftpPassword = grunt.option('ftpPassword');		
+		
+		var contents = '{"key1":{"username":"' + ftpUsername + '", "password":"' + ftpPassword + '"}}';
+			
+		// Create a file to supply the authentication parameters to the deployment
+		grunt.file.write('.ftppass', contents);
+	});
+};
