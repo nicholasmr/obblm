4fed65504697a4ce92199512d77292ef30f8255e
Shteve0 <arthurs.steve@googlemail.com>
2016-12-08 13:21:33 +1300

    Update game_data_bb16.php
    Add roster and star changes from LRB6 to BB16

--- lib/game_data_bb16.php
+++ lib/game_data_bb16.php
@@ -21,36 +21,66 @@
  *
  */
 
-require_once('lib/game_data_lrb6x.php');
+require_once('lib/game_data_lrb6.php');
 
-// Changes to present teams/positionals from LRB5b to LRB6.
+// New skills added in BB16.
 
-$DEA['Necromantic']['players']['Flesh Golem']['cost'] = 110000;
-if(isset($DEA['Necromantic']['players']['Werewolf']))
-    $DEA['Necromantic']['players']['Necromantic Werewolf'] = $DEA['Necromantic']['players']['Werewolf']; unset($DEA['Necromantic']['players']['Werewolf']);
-if(isset($DEA['Norse']['players']['Ulfwerener']))
-    $DEA['Norse']['players']['Norse Werewolf'] = $DEA['Norse']['players']['Ulfwerener']; unset($DEA['Norse']['players']['Ulfwerener']);
-if(isset($DEA['Norse']['players']['Snow Troll']))
-    $DEA['Norse']['players']['Yhetee'] = $DEA['Norse']['players']['Snow Troll']; unset($DEA['Norse']['players']['Snow Troll']);
-if(isset($DEA['Norse']['players']['Runner']))
-    $DEA['Norse']['players']['Catcher'] = $DEA['Norse']['players']['Runner']; unset($DEA['Norse']['players']['Runner']);
-if(isset($DEA['Norse']['players']['Berserker']))
-    $DEA['Norse']['players']['Blitzer'] = $DEA['Norse']['players']['Berserker']; unset($DEA['Norse']['players']['Berserker']);
+$skillarray['E'][114] = $skillididx[114] = 'Monstrous Mouth';
+$skillarray['E'][115] = $skillididx[115] = 'Timmm-ber!';
+$skillarray['E'][116] = $skillididx[116] = 'Weeping Blades';
 
-// Changes in star players from LRB5b to LRB6.
+// Changes to present teams/positionals from LRB6 to BB16.
 
-$stars['Bertha Bigfist']['cost']                      = 290000;
-$stars['Crazy Igor']['cost']                          = 120000;
-$stars['Dolfar Longstride']['cost']                   = 150000;
-$stars['Fezglitch']['cost']                           = 100000;
-$stars['Glart Smashrip Jr.']['cost']                  = 210000;
-$stars['Morg \'n\' Thorg']['cost']                    = 430000;
-$stars['Zzharg Madeye']['cost']                       = 90000;
-$stars['Deeproot Strongbranch']['cost']               = 300000;
-$stars['Eldril Sidewinder']['cost']                   = 200000;
-$stars['Ramtut III']['cost']                          = 380000;
+$DEA['Human']['players']['Catcher']['cost'] = 60000;
+$DEA['Skaven']['players']['Gutter Runner']['def'] = array (23, 116);
+$DEA['Halfling']['players']['Treeman']['def'] = array (54, 57, 58, 109, 59, 110, 115);
 
-// Changes to inducements
+// New star players in BB16.
 
-$inducements['Halfling Master Chef']['reduced_cost'] = 100000; # Old LRB5 value.
-
+$stars['Rasta Tailspike'] = array (
+       'id'            => -47,
+       'ma'            => 8,
+       'st'            => 3,
+       'ag'            => 3,
+       'av'            => 7,
+       'def'    => array (99, 20, 73),
+       'cost'          => 110000,
+       'icon'          => 'star',
+       'races'         => array(19),
+   );
+   
+$stars['Frank N Stein'] = array (
+       'id'            => -48,
+       'ma'            => 4,
+       'st'            => 5,
+       'ag'            => 1,
+       'av'            => 9,
+       'def'    => array (99, 50, 54, 103, 57, 59),
+       'cost'          => 210000,
+       'icon'          => 'star',
+       'races'         => array(9, 13, 17),
+   );
+   
+$stars['Bilerot Vomitflesh'] = array (
+       'id'            => -49,
+       'ma'            => 4,
+       'st'            => 5,
+       'ag'            => 2,
+       'av'            => 9,
+       'def'    => array (99, 3, 72, 74),
+       'cost'          => 180000,
+       'icon'          => 'star',
+       'races'         => array(15, 1),
+   );
+   
+$stars['Guffle Pusmaw'] = array (
+       'id'            => -50,
+       'ma'            => 5,
+       'st'            => 3,
+       'ag'            => 4,
+       'av'            => 9,
+       'def'    => array (99, 74, 101, 114),
+       'cost'          => 220000,
+       'icon'          => 'star',
+       'races'         => array(15),
+   );
